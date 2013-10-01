47899c4ec155a1dc82f1c9644f79a69a0f2dff6a
Sergey Kashin <sergey.don.kashin@gmail.com>
2013-10-02 03:31:00 +0400

    Now looks like fully random Sudoku generation is done.

--- Sudoku/src/com/example/sudoku/SudokuAdapter.java
+++ Sudoku/src/com/example/sudoku/SudokuAdapter.java
@@ -14,25 +14,30 @@ import android.widget.TextView;
  * field values  
  *
  */
-public class SudokuAdapter extends BaseAdapter {
+public class SudokuAdapter extends BaseAdapter
+{
 	
 	static String TAG = "SudokuAdapter";
 	private Context mContext;
 
-	public SudokuAdapter(Context context) {
+	public SudokuAdapter(Context context)
+	{
 		regenerateSudokuArray();
 		mContext = context;
 		mTextViews = new TextView[9][9];
 	}
 	
 	@Override
-	public int getCount() {
+	public int getCount()
+	{
 		return 81;
 	}
 
 	@Override
-	public Object getItem(int position) {
-		if (position >= mFieldValues.length) {
+	public Object getItem(int position)
+	{
+		if (position >= mFieldValues.length)
+		{
 			android.util.Log.e(TAG, "getItem: position " + position +" is out of range");
 			return null;
 		}
@@ -40,60 +45,82 @@ public class SudokuAdapter extends BaseAdapter {
 	}
 
 	@Override
-	public long getItemId(int position) {
-		if (position >= 81) {
+	public long getItemId(int position)
+	{
+		if (position >= 81)
+		{
 			android.util.Log.e(TAG, "getItem: position " + position +" is out of range");
 			return 0;
 		}
 		if (mTextViews == null ||
-		    mTextViews[position % 9][position / 9] == null) {
+		    mTextViews[position % 9][position / 9] == null)
+		{
 			return 0;
 		}
 		return mTextViews[position % 9][position / 9].getId();
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
 		TextView newView;
     	int column = position / 9;
     	int row = position % 9;
-        if (convertView == null) {  // if it's not recycled, initialise some attributes
+        if (convertView == null)
+        {  // if it's not recycled, initialise some attributes
         	newView = new TextView(mContext);
         	newView.setText(mFieldValues[row][column].toString());
-        } else {
+        }
+        else
+        {
             newView = (TextView) convertView;
         }
         mTextViews[row][column] = newView;
 		return newView;
 	}
 	
-	
-	public void regenerateSudokuArray() {
+	/**
+	 * @brief regenerates Sudoku field == regenerates Field's and User's Integer arrays.
+	 */
+	public void regenerateSudokuArray()
+	{
 		// TODO: implement field generation :)
 		mFieldValues = new Integer[9][9];
 		mUserValues = new Integer[9][9];
 		Random random = new Random();
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
 				mUserValues[i][j] = 0;
 			}
 		}
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
 				int newValue = random.nextInt(9) + 1;
-				android.util.Log.e(TAG, "generateField. newValue=" + newValue + " i=" + i + " j=" + j);
 				ArrayList<Integer> excludedValues = new ArrayList<Integer>();
+				int regionsColumn = i / 3 + 1;
+				int regionsRow = j / 3 + 1;
 				while (hasSameValueInColumn(newValue, j) ||
-					   hasSameValueInRow(newValue, i)) {
+					   hasSameValueInRow(newValue, i) ||
+					   hasSameValueInRegion(newValue, regionsColumn, regionsRow))
+				{
 					excludedValues.add(newValue);
 					newValue = random.nextInt(9) + 1;
 					boolean needToRegenerate = false;
-					while (excludedValues.contains(newValue)) {
+					while (excludedValues.contains(newValue))
+					{
 						newValue++;
-						if (newValue > 9) {
-							if (!needToRegenerate) {
+						if (newValue > 9)
+						{
+							if (!needToRegenerate)
+							{
 								needToRegenerate = true;
-							} else {
+							}
+							else
+							{
 								regenerateSudokuArray();
 								return;
 							}
@@ -101,7 +128,6 @@ public class SudokuAdapter extends BaseAdapter {
 						}
 					}
 				}
-				android.util.Log.e(TAG, "generateField. added newValue=" + newValue);
 				mFieldValues[i][j] = newValue;
 				mUserValues[i][j] = newValue;
 			}
@@ -110,31 +136,60 @@ public class SudokuAdapter extends BaseAdapter {
 	
 	public boolean hasSameValueInRow(Integer value, int row) {
 		boolean res = false;
-		for (int i = 0; i < 9; i++) {
-			if (value.intValue() == mUserValues[row][i]) {
+		for (int i = 0; i < 9; i++)
+		{
+			if (value.intValue() == mUserValues[row][i])
+			{
 				return true;
 			}
 		}
 		return res;
 	}
 	
-	public boolean hasSameValueInColumn(Integer value, int column) {
+	public boolean hasSameValueInColumn(Integer value, int column)
+	{
 		boolean res = false;
-		for (int i = 0; i < 9; i++) {
-			if (value.intValue() == mUserValues[i][column]) {
+		for (int i = 0; i < 9; i++)
+		{
+			if (value.intValue() == mUserValues[i][column])
+			{
 				return true;
 			}
 		}
 		return res;
 	}
 	
-	public boolean isUserWin() {
-		if (mFieldValues.length != mUserValues.length) {
+	public boolean hasSameValueInRegion(Integer value, int regionsColumn, int regionsRow)
+	{
+		boolean res = false;
+		int iEnd = regionsColumn * 3 - 4;
+		int jEnd = regionsRow * 3 - 4;
+		for (int i = regionsColumn * 3 - 1; i > iEnd; i--)
+		{
+			for (int j = regionsRow * 3 - 1; j > jEnd; j-- )
+			{
+				if (value.intValue() == mUserValues[i][j])
+				{
+					return true;
+				}
+			}
+		}
+
+		return res;
+	}
+
+	public boolean isUserWin()
+	{
+		if (mFieldValues.length != mUserValues.length)
+		{
 			return false;
 		}
-		for (int i = 0; i < 9 ; i++) {
-			for (int j = 0; j < 9 ; j++) {
-				if (mFieldValues[i][j] != mUserValues[i][j]) {
+		for (int i = 0; i < 9 ; i++)
+		{
+			for (int j = 0; j < 9 ; j++)
+			{
+				if (mFieldValues[i][j] != mUserValues[i][j])
+				{
 					return false;
 				}
 			}
@@ -142,11 +197,13 @@ public class SudokuAdapter extends BaseAdapter {
 		return true;
 	}
 	
-	public Integer[][] getUserValues() {
+	public Integer[][] getUserValues()
+	{
 		return mUserValues;
 	}
 	
-	public void setUserValues(Integer[][] userValues) {
+	public void setUserValues(Integer[][] userValues)
+	{
 		mUserValues = userValues;
 	}
 	
